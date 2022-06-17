f496e0241e711e2d8317bc3cbe55870e17c36f61
Douglas Soares <douglassoares16.dev@gmail.com>
2022-06-17 14:23:08 -0300

    Updated 'memo' in MovieCard Component

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -37,5 +37,5 @@ function MovieCardComponent(props: MovieCardProps) {
 }
 
 export const MovieCard = memo(MovieCardComponent, (prevProps, nextProps) => {
-  return Object.is(prevProps, nextProps);
+  return prevProps.title === nextProps.title;
 });
